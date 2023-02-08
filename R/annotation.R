45df02556c54d6f4088edab4c9a48cd9da3e5c38
mhkowalski <madelinekowalski@gmail.com>
2023-02-08 16:28:30 -0500

    update vignette

---
+++ R/annotation.R
@@ -0,0 +1,103 @@
+
+
+#' Overlap PAS with polyA db 
+#' @param object Seurat object containing a polyAsiteAssay
+#' @param assay Name of polyAsiteAssay
+#' @param polyAdb.file Location to find polyAdbv3 file 
+#' @param max.dist Keep sites within this distance to a polyAdbv3 site. Default is 50 nucleotides. 
+#' 
+#' @importFrom GenomicRanges makeGRangesFromDataFrame 
+#' @importFrom IRanges distanceToNearest trim
+#' @importFrom S4Vectors queryHits subjectHits
+#' @importFrom BiocGenerics strand
+#' @importFrom plyranges anchor_3p mutate
+#' @importFrom GenomeInfoDb seqlevelsStyle
+#' 
+#' @return Seurat object with annotated meta.features for polyAsiteAssay. Keeps all features 
+#' in original matrix, but annotations columns will be populated with NAs if a feature does not 
+#' overlap with a polyAdbv3 polyA site within the distance specified my max.dist.
+#' @concept annotation
+#' @export
+#' 
+GetPolyADbAnnotation <- function(
+  object, 
+  assay = "polyA",
+  polyAdb.file = NULL, #
+  max.dist = 50) 
+{
+  #readin polyAdbv3 and make GRanges file 
+  if (!( file.exists(polyAdb.file))) {
+    stop("Please check that you have specified the location of polyAdb.file correctly")
+  }
+  
+  anno <- read.table(file = polyAdb.file, header = TRUE)
+  GR.polyA.db = makeGRangesFromDataFrame( anno, 
+                                          keep.extra.columns = TRUE, 
+                                          seqnames.field = "hg38_Chromosome_format",
+                                          start.field = "hg38_Position", 
+                                          end.field = "hg38_Position",
+                                          strand.field = "Strand")
+  
+  if( !assay %in% Assays(object) ){
+    stop(paste0(assay," assay is not present in object"))
+  }
+  if( !class(object[[assay]]) == "polyAsiteAssay"){
+    stop(paste0(assay," assay is not a polyAsiteAssay"))
+  }
+  if( !assay == DefaultAssay(object)){
+    DefaultAssay(object) <- assay
+    message(paste0("Setting default assay to ",assay))
+  }
+  ranges = slot(object = object[[assay]], name = "ranges")
+  if ( ! "strand" %in% colnames(object[[assay]]@meta.features)){
+    object[[assay]] <- AddMetaData(object = object[[assay]] , metadata = as.character(strand(ranges)) , col.name = "strand")
+  }
+  #why include this? 
+  #BiocGenerics::strand(ranges) <- S4Vectors::Rle(object[[assay]][["strand"]][,1])
+  #mcols(ranges)$rn <- rownames(object[[assay]])
+  if (  "*" %in% unique(strand(ranges)) ){
+    if( table(strand(ranges))["*"] != 0 ){
+      stop("Exiting. Cannot annotate unstranded PAS. Please remove unstranded PAS.")
+    }
+  } 
+  if( !all(seqlevelsStyle(ranges) == seqlevelsStyle(GR.polyA.db)) ) {
+    if( seqlevelsStyle(ranges)[1] == "UCSC"){
+      warning("\n Annotation does not match between ranges and polyAdb.\n Annotation set to USCS.\n")
+      seqlevelsStyle(GR.polyA.db) <- "UCSC"
+    }else{
+      warning("\n Annotation does not match between ranges and polyAdb.\n Annotation set to Ensembl.\n")
+      seqlevelsStyle(GR.polyA.db) <- "Ensembl"
+    }
+  }
+  GR = anchor_3p(ranges)
+  GR_cleavage = mutate(GR, width = 1)
+  #GR = trim(GR)
+  #gr = ranges %>% anchor_3p() %>% mutate(width=1) %>% trim()
+  OL = suppressWarnings(distanceToNearest(x = GR_cleavage, subject = GR.polyA.db,
+                                          ignore.strand=FALSE))
+  keep <- dplyr::filter(as.data.frame(OL), distance<= max.dist)
+  
+  #write this more to handle if htere are 2 sites equidistant
+  if (sum(duplicated(queryHits(OL))) >0 ) {
+    dups <- OL[duplicated(queryHits(OL))]
+    
+  }
+
+  #re-write this 
+  peak.df <- data.frame(GR)
+  peak.df <- peak.df[,c("seqnames", "start", "end", "width", "strand")]
+  peak.df$peak <- rownames(object[[assay]])
+
+  #add information about what feature in object mathces polyAdbv3 peaks
+  tmp <- peak.df[keep$queryHits,]
+  tmp2 <- data.frame(GR.polyA.db)[keep$subjectHits,]
+  tmp2 <- tmp2[,!(names(tmp2) %in%  c("strand", "seqnames", "start", "end", "width"))]
+  tmp3 <- cbind(tmp, tmp2)
+  
+  meta.new <- left_join(peak.df, tmp3, by = c("seqnames", "start", "end", "strand", "width", "peak"))
+  rownames(meta.new) <- meta.new$peak
+  for( i in 1:ncol(meta.new)){
+    object[[assay]] <- AddMetaData(object = object[[assay]] , metadata = meta.new[,colnames(meta.new)[i]] , col.name = colnames(meta.new)[i] )
+  }
+  return(object)
+}
---
+++ R/preprocessing.R
@@ -0,0 +1,164 @@
+
+#' The function reads in output from polyApipe and returns a count matrix
+#' @param counts.file file containing the counts from polyApipe in .tsv format
+#' @param peaks.file gff file that requires the seqid, start, end, and strand of each peak
+#' @param sep Separators
+#' @param filter.chromosomes If TRUE (default), only include main chromosomes. 
+#' @param min.features Cell must have at least this many feature to be included.
+#' @param min.cells A feature must be present in at least this manhy cells.
+#' @param verbose Print output.
+#'
+#' @return Returns count matrix with peaks in the form "chromosome:start,end:strand"
+#'  such as "10:100000560,100000859,100000560,100000859:+"
+#'
+#'
+#' @importFrom data.table fread
+#' @importFrom Matrix sparseMatrix colSums rowSums
+#' @importFrom rtracklayer readGFF
+#'
+#' @export
+#' @concept preprocessing
+ReadPolyApipe <- function(counts.file, peaks.file = NULL, sep = c(":",",",":"),
+                          filter.chromosomes = TRUE, min.features = NULL, min.cells = NULL , 
+                          verbose=TRUE)  {
+
+  if (verbose) {
+    message("Reading count file ...")
+  }
+  the_counts <- fread( file = counts.file, sep = "\t", header=TRUE,
+                                   showProgress = verbose,
+                                   colClasses = c("factor","factor", "numeric"),
+                                   nrows=-1)
+  colnames(the_counts) <- c("peak","cell","count")
+
+  # First construct the sparse matrix of all the counts
+  if (verbose) {
+    message("Creating sparse matrix ...")
+  }
+  counts_matrix <- sparseMatrix(
+    i = as.integer(the_counts$peak),
+    j = as.integer(the_counts$cell),
+    x = as.integer(the_counts$count))
+  colnames(counts_matrix) <- levels(the_counts$cell)
+  rownames(counts_matrix) <- levels(the_counts$peak)
+
+  if(! is.null( peaks.file )){
+    if (verbose) {
+      message("Reading peak file ...")
+    }
+    the_peaks = readGFF( filepath = peaks.file)
+    the_peaks$peak <- gsub( '"', '', the_peaks$peak)
+    m = match( rownames(counts_matrix)  , the_peaks$peak )
+    if ( length (which( is.na(m) == TRUE )) > 0 ){
+      if ( length(which( is.na(m) == TRUE )) == nrow(counts_matrix) ){
+        stop("Exiting. peaks.file does not match count_matrix rownames.")
+      }else{
+        n = nrow(counts_matrix)
+        del = which(! rownames(counts_matrix) %in% the_peaks$peak )
+        idx = which( rownames(counts_matrix) %in% the_peaks$peak )
+        counts_matrix = counts_matrix[idx,]
+        warning(paste0( "Removed ",length(del)," of ", n ," peaks not present in peaks.file"))
+        m = match( rownames(counts_matrix)  , the_peaks$peak ) #update
+
+        the_peaks$newRN = apply( the_peaks[,c("seqid","start", "end", "strand")], 1,FUN =
+                                   function(x){ paste0(x[1], sep[1],x[2], sep[2], x[3], sep[3], x[4]   )} )
+
+
+      }
+    }else{
+      the_peaks$newRN = apply( the_peaks[,c("seqid","start", "end", "strand")], 1,FUN =
+                                 function(x){ paste0(x[1], sep[1],x[2], sep[2], x[3], sep[3], x[4]    )} )
+      rownames(counts_matrix) <- the_peaks$newRN[m]
+    }
+  }
+  
+  if(!is.null(min.features)){
+    rs = rowSums( counts_matrix > 0 )
+    del = which(rs < min.features)
+    keep = which(rs >= min.features)
+    counts_matrix = counts_matrix[keep,]
+    if (verbose) {
+      message(paste0("Removed ",length(del)," feature(s) covered in less than ",min.features," cells ..."))
+    }
+  }
+  
+  if(!is.null(min.cells)){
+    cs = colSums( counts_matrix > 0 )
+    del = which(cs < min.cells)
+    keep = which(cs >= min.cells)
+    counts_matrix = counts_matrix[,keep]
+    if (verbose) {
+      message(paste0("Removed ",length(del)," cell(s) with less than ",min.cells," features ..."))
+    }
+  }
+  
+  if (filter.chromosomes) {
+    chr = sapply(rownames(counts_matrix) , FUN = function(j) { strsplit(j,":")[[1]][1]})
+    accept = c(paste0("^",c(c(1:23),"X","Y","MT","M")),
+               paste0("chr",c(c(1:23),"X","Y","MT","M")))
+    del = grep(paste0(accept,collapse = "|"),chr, invert = T)
+    keep = grep(paste0(accept,collapse = "|"),chr, invert = F)
+    counts_matrix = counts_matrix[keep,]
+    if (verbose) {
+      message("Removed ", length(del), " entries falling on scaffolds")
+    }
+  }
+  
+  if (verbose) {
+  message("Loaded ", nrow(counts_matrix), " x ", ncol(counts_matrix), " feature by cell matrix")
+  }
+  
+  return(counts_matrix)
+}
+
+
+
+#' Finds variable features based on polyA residuals
+#' @param object Object containing a polyAsite assay
+#' @param nfeatures Number of features to select as top variable features.
+#' @param gene.names Column in meta features that contains gene names. At most one feature per gene will be selected.
+#' @param method How to calculate polyA residuals. If method = "residuals", will use select the most variable in 
+#' each gene and then pick the top nfeatures.
+#' 
+#' @rdname FindVariableFeatures
+#' @importFrom utils head tail
+#' @concept preprocessing
+#' @export
+#' @method FindVariableFeatures polyAsiteAssay
+
+FindVariableFeatures.polyAsiteAssay <- function(
+    object,
+    nfeatures = 2000,
+    selection.method = "residuals",
+    gene.names = "symbol",
+    ...)
+  {
+  if (selection.method == "residuals") {
+    
+    if (dim(GetAssayData(object, slot="scale.data"))[1] == 0)  {
+      stop ("No features found in scale.data slot. Run CalcPolyAResiduals prior to FindVariableFeatures.")
+    }
+    
+    if (!(gene.names %in% colnames(object[[]]))) {
+      stop("Gene.names column not found in meta.features, please make sure 
+         you are specific gene.names correctly")
+    }
+    
+    var <- data.frame(var = apply(object@scale.data, 1, function(x) var(x[x != 0])))
+    var$symbol <- object[[]][rownames(var), gene.names]
+    var <- var[order(var$var, decreasing = TRUE), ] #sort by maximum variance 
+    var.unique <- var[!duplicated(var$symbol),]
+    
+    nfeatures <- min(nfeatures, nrow(x = nrow(var.unique)))
+  
+    VariableFeatures(object) <- head(rownames(var.unique), n=nfeatures)
+  } else {
+    assay <- as(object = object[[DefaultAssay(object)]], Class = "ChromatinAssay")
+    tmp <- Seurat::FindVariableFeatures(assay, nfeatures = nfeatures, selection.method = selection.method, ...)
+    VariableFeatures(object) <- VariableFeatures(tmp)
+  }
+  return(object)
+}
+
+
+
